6540497c28cab1cb8b3ffeb5208ee14bdce8a214
Olivia James <a@company.com>
2020-08-13 17:40:36 -0700

    Initial commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Olivia James migrated on 8/13/20.
+//  Copyright © 2020 Olivia James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
